886611fbf603544a390411240caa2e6dc6952c44
rlawjdcks <rlawjdcks28@gmail.com>
2024-03-07 17:11:15 +0900

    20240307

--- _sp0305_2/src/main/java/main/MainForSpring.java
+++ _sp0305_2/src/main/java/main/MainForSpring.java
@@ -10,9 +10,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import config.AppCtx;
 import spring.ChangePasswordService;
 import spring.DuplicateMemberException;
+import spring.MemberInfoPrinter;
+import spring.MemberListPrinter;
 import spring.MemberNotFoundException;
 import spring.MemberRegisterService;
 import spring.RegisterRequest;
+import spring.VersionPrinter;
 import spring.WrongIdPasswordException;
 
 public class MainForSpring {
@@ -37,9 +40,42 @@ public class MainForSpring {
 			} else if (command.startsWith("change ")) {
 				processChangeCommand(command.split(" "));
 				continue;
-			} 
+			}  else if (command.equals("list")) {
+				processListCommand();
+				continue;
+			} else if (command.startsWith("info ")) {
+				processInfoCommand(command.split(" "));
+				continue;
+			} else if (command.equals("version")) {
+				processVersionCommand();
+				continue;
+			}
 			printHelp();
 		}
+	}
+	private static void processVersionCommand() {
+		VersionPrinter versionPrinter = 
+				ctx.getBean("versionPrinter", VersionPrinter.class);
+		versionPrinter.print();
+	}
+	
+	private static void processInfoCommand(String[] arg) {
+		if (arg.length != 2) {
+			printHelp();
+			return;
+		}
+		MemberInfoPrinter infoPrinter = 
+				ctx.getBean("infoPrinter", MemberInfoPrinter.class);
+		infoPrinter.printMemberInfo(arg[1]);
+		
+	}
+
+
+	private static void processListCommand() {
+		MemberListPrinter listPrinter = 
+				ctx.getBean("listPrinter", MemberListPrinter.class);
+		listPrinter.printAll();
+		
 	}
 
 	private static void processNewCommand(String[] arg) {
@@ -93,6 +129,6 @@ public class MainForSpring {
 		System.out.println();
 	}
 
-
+	
 
 }
---
+++ _sp0305_2/src/main/java/spring/MemberInfoPrinter.java
@@ -0,0 +1,22 @@
+package spring;
+
+import lombok.Setter;
+
+@Setter
+public class MemberInfoPrinter {
+
+		private MemberDao memberDao;
+		private MemberPrinter printer;
+		
+		public void printMemberInfo(String email) {
+			Member member = memberDao.selectByEmail(email);
+			if (member == null) {
+				System.out.println("데이터 없음\n");
+				return;
+			}
+			printer.print(member);
+			System.out.println();
+		}
+
+
+}
